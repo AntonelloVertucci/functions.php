8549c183a5ce48689fd4f3c7aec065e5e7c226b1
AntonelloVertucci <52776708+AntonelloVertucci@users.noreply.github.com>
2019-07-31 15:12:37 +0200

    Update enfold.php

--- enfold.php
+++ enfold.php
@@ -1,65 +1,69 @@
 <?php 
 
-// Remove Avia Framework debug information 
+/* Debug modus */
+function builder_set_debug(){
+  return "debug";
+}
+add_action('avia_builder_mode', "builder_set_debug");
+
+/* Remove Avia Framework debug information */
 if(!function_exists('avia_debugging_info')){
   function avia_debugging_info() {}
 }
 
-// Remove the Import Dummy Data button (Demo)
+/* Remove the Import Dummy Data button (Demo) */
 add_theme_support('avia_disable_dummy_import');
 
-// Backend Style 
-add_action('admin_head', 'my_custom_enfold');
-function my_custom_enfold() {
+/* Backend Style */
+function my_custom_enfold(){
   echo '<style>
     #pr-logo{display:none}
     .avia_options_page_sidebar .avia_header{background-size:60px; background-position:center; background-image: url('. get_stylesheet_directory_uri() .'/img/n-logo.png); background-repeat:no-repeat}
     } 
   </style>';
 }
+add_action('admin_head', 'my_custom_enfold');
 
-// Shortcode Display Breadcrump 
+/* Shortcode Display Breadcrump */
 function nm_breadcrump_func( $atts ){
     global $avia_config;
     return avia_breadcrumbs(array('separator' => '/', 'before' => 'Sie befinden sich hier', 'richsnippet' => true));
 }
 add_shortcode( 'nm_breadcrump', 'nm_breadcrump_func' );
 
-// Breadcrump remove Trail before
-add_filter('avia_breadcrumbs_args', 'remove_trail_before', 50, 1);
+/* Breadcrump remove Trail before */
 function remove_trail_before($args){
     $args['before'] = '';
     return $args;
 }
+add_filter('avia_breadcrumbs_args', 'remove_trail_before', 50, 1);
 
-// Replace Portfolio name in menu admin
-add_filter('avf_portfolio_cpt_args', 'avf_portfolio_cpt_args_mod');
-function avf_portfolio_cpt_args_mod($args) {
+/* Replace Portfolio name in menu admin */
+function avf_portfolio_cpt_args_mod($args){
     $args['labels']['name'] = 'Produkte';
     return $args;
 }
+add_filter('avf_portfolio_cpt_args', 'avf_portfolio_cpt_args_mod');
 
-// Modify/Overwrite Enfold shortcode
-add_filter('avia_load_shortcodes', 'avia_include_shortcode_template', 15, 1);
-function avia_include_shortcode_template($paths)
-{
+/* Modify/Overwrite Enfold shortcode */
+function avia_include_shortcode_template($paths){
     $template_url = get_stylesheet_directory();
-        array_unshift($paths, $template_url.'/shortcodes/');
-
+    array_unshift($paths, $template_url.'/shortcodes/');
     return $paths;
 }
+add_filter('avia_load_shortcodes', 'avia_include_shortcode_template', 15, 1);
 
-// Remove time from the enfold latest news widget
-add_filter('avia_widget_time', 'change_avia_date_format', 10, 2);
-function change_avia_date_format($date, $function) {
+/* Remove time from the enfold latest news widget */
+function change_avia_date_format($date, $function){
   $output = get_option('date_format');
   return $output;
 }
+add_filter('avia_widget_time', 'change_avia_date_format', 10, 2);
 
-// Disable Portfolio 
-add_action('after_setup_theme', 'remove_portfolio');
-function remove_portfolio() {
+/* Disable Portfolio */
+function remove_portfolio(){
     remove_action('init', 'portfolio_register');
 }
+add_action('after_setup_theme', 'remove_portfolio');
 
 ?>
