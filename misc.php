2a439551f8aa2e77c554d4eda960fcec5a5fb376
AntonelloVertucci <52776708+AntonelloVertucci@users.noreply.github.com>
2020-07-28 10:04:23 +0200

    Update misc.php

--- misc.php
+++ misc.php
@@ -94,3 +94,14 @@ function custom_wp_targeted_link_rel($rel_values){
 add_filter('wp_targeted_link_rel', 'custom_wp_targeted_link_rel',999);
 
 
+/*
+    =============================================
+    Hide update notifications
+    =============================================
+*/
+function remove_core_updates(){
+	global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
+add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
+add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
